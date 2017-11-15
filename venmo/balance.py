b63190461c8d2f5c5613413ab9fadc3a12550115
CatherineZeng <yczeng@mit.edu>
2017-11-15 10:41:46 -0500

    added configuration prompt

--- venmo/balance.py
+++ venmo/balance.py
@@ -1,14 +1,21 @@
 '''
 Balance module.
 '''
+import logging
 import json
-
 import requests
-
 import venmo
 
+logger = logging.getLogger('venmo.payment')
+
 def balance():
-    access_token = venmo.auth.get_access_token()
+    access_token = venmo.auth.get_access_token() 
+    if not access_token: 
+        logger.warn('No access token. Configuring ...') 
+        if not venmo.auth.configure(): 
+            return 
+        access_token = venmo.auth.get_access_token() 
+
     response = requests.get('https://api.venmo.com/v1/me?access_token=' + access_token)
     response_dict = response.json()
     print(response_dict['data']['balance'])
