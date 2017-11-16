dec31c93f4dc2079616ae2926a2c0b2c6fe4a15c
CatherineZeng <yczeng@mit.edu>
2017-11-16 13:10:14 -0500

    tailing new lines

--- venmo/balance.py
+++ venmo/balance.py
@@ -6,7 +6,9 @@ import json
 import requests
 import venmo
 
+
 logger = logging.getLogger('venmo.payment')
+
 
 def balance():
     access_token = venmo.auth.get_access_token() 
@@ -18,5 +20,6 @@ def balance():
 
     response = requests.get('https://api.venmo.com/v1/me?access_token=' + access_token)
     response_dict = response.json()
+
     print(response_dict['data']['balance'])
     return response_dict['data']['balance']
